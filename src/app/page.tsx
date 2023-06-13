6e7042cfbc9c71cdd6f40b03fb8459ecdbe289f8
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-06-13 01:20:00 -0400

    more quotes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import QuoteList from "@components/QuoteList";
 export default async function Page() {
     const data: InitialData[] = await getInitialData();
     return (
-        <>
+        <div className={"sm:p-6"}>
             <div className={"flex flex-col items-center"}>
                 <h1 className={"text-5xl font-bold"}>Quotes (WIP)</h1>
             </div>
@@ -16,6 +16,6 @@ export default async function Page() {
                     )
                 })}
             </div>
-        </>
+        </div>
     )
 }
