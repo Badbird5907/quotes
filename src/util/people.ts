59f40e19db472c3016fad5bc0d1990e84ea38fdc
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-06-15 09:14:46 -0400

    More quotes

--- src/util/people.ts
+++ src/util/people.ts
@@ -2,9 +2,11 @@ import ryan from "../../quotes/ryan.json";
 import aaron from "../../quotes/aaron.json";
 import coin from "../../quotes/coin.json";
 import duper from "../../quotes/duper.json";
+import connor from "../../quotes/connor.json";
 export const people = [
     {name: "coin", data: coin},
     {name: "duper", data: duper},
     {name: "ryan", data: ryan},
     {name: "aaron", data: aaron},
+    {name: "connor", data: connor},
 ]
